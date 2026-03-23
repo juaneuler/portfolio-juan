2da6082e4755b9503c24e5a5542be1cf8f9c069f
juaneuler <juaneuler@hotmail.com>
2026-03-23 16:59:00 -0300

    feat(tecnologias): se agregó Google Tag Manager y nuevo logo SVG al listado de tecnologías

--- src/data/tecnologias.js
+++ src/data/tecnologias.js
@@ -1,60 +1,35 @@
 // Este archivo tiene un array de objetos. Cada objeto tiene las propiedades a renderizar en el componente Tecnologías.jsx
 
-// Importación de las imágenes
-
-import htmlLogo from "../assets/html5Logo.svg"
-import cssLogo from "../assets/css3Logo.svg"
-import bootstrapLogo from "../assets/bootstrapLogo.svg"
-import sassLogo from "../assets/sassLogo.svg"
-import jsLogo from "../assets/javascriptLogo.svg"
-import tsLogo from "../assets/typescriptLogo.svg"
 import reactLogo from "../assets/reactLogo.svg"
-import reactBootstrapLogo from "../assets/reactBootstrapLogo.svg"
 import angularLogo from "../assets/angularLogo.svg"
-import rxjsLogo from "../assets/rxjsLogo.svg"
-import ngrxLogo from "../assets/ngrxLogo.svg"
-import reduxLogo from "../assets/reduxLogo.svg"
-import viteLogo from "../assets/viteLogo.svg"
-import vitestLogo from "../assets/vitestLogo.svg"
-import karmaLogo from "../assets/karmaLogo.svg"
-import jasmineLogo from "../assets/jasmineLogo.svg"
+import aemLogo from "../assets/aemLogo.svg"
+import gtmLogo from "../assets/gtmLogo.svg"
+import tsLogo from "../assets/typescriptLogo.svg"
+import jsLogo from "../assets/javascriptLogo.svg"
+import pythonLogo from "../assets/pythonLogo.svg"
+import sassLogo from "../assets/sassLogo.svg"
 import firebaseLogo from "../assets/firebaseLogo.svg"
 import gitLogo from "../assets/gitLogo.svg"
-import githubLogo from "../assets/githubLogo.svg"
 import wpLogo from "../assets/wpLogo.svg"
 import elementorLogo from "../assets/elementorLogo.svg"
 import wcLogo from "../assets/wcLogo.svg"
-import aemLogo from "../assets/aemLogo.svg"
-import pythonLogo from "../assets/pythonLogo.svg"
-import framerMotionLogo from "../assets/framerMotionLogo.svg"
-
-
-// Exportación del array para usar en el componente
+import reduxLogo from "../assets/reduxLogo.svg"
+import rxjsLogo from "../assets/rxjsLogo.svg"
 
 export const tecnologias = [
     { nombre: "React", logo: reactLogo },
     { nombre: "Angular", logo: angularLogo },
     { nombre: "AEM", logo: aemLogo },
+    { nombre: "Google Tag Manager", logo: gtmLogo },
     { nombre: "TypeScript", logo: tsLogo },
     { nombre: "JavaScript", logo: jsLogo },
     { nombre: "Python", logo: pythonLogo },
-    { nombre: "HTML5", logo: htmlLogo },
-    { nombre: "CSS3", logo: cssLogo },
     { nombre: "SASS", logo: sassLogo },
-    { nombre: "Framer Motion", logo: framerMotionLogo },
-    { nombre: "Bootstrap", logo: bootstrapLogo },
-    { nombre: "React Bootstrap", logo: reactBootstrapLogo },
+    { nombre: "Redux", logo: reduxLogo },
+    { nombre: "RxJS", logo: rxjsLogo },
+    { nombre: "Firebase", logo: firebaseLogo },
     { nombre: "WordPress", logo: wpLogo },
     { nombre: "Elementor", logo: elementorLogo },
     { nombre: "WooCommerce", logo: wcLogo },
-    { nombre: "Git", logo: gitLogo },
-    { nombre: "GitHub", logo: githubLogo },
-    { nombre: "Firebase", logo: firebaseLogo },
-    { nombre: "Vite", logo: viteLogo },
-    { nombre: "Vitest", logo: vitestLogo },
-    { nombre: "Karma", logo: karmaLogo },
-    { nombre: "Jasmine", logo: jasmineLogo },
-    { nombre: "RxJS", logo: rxjsLogo },
-    { nombre: "NgRx", logo: ngrxLogo },
-    { nombre: "Redux", logo: reduxLogo }
+    { nombre: "Git", logo: gitLogo }
 ]
